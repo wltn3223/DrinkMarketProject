89a650f53243d6249b0ad8b13c6ebc46c85ffad5
Mun Jisu <answltn8@gmail.com>
2023-10-12 09:16:24 +0900

    사용되지 않는 문장 삭제

--- src/cart/CartServiceImple.java
+++ src/cart/CartServiceImple.java
@@ -6,7 +6,6 @@ import item.Drink;
 import user.Customer;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Optional;
